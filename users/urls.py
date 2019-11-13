99ab8987e1146d055d2984a92b17e887ce22287f
Leo <leoamby@gmail.com>
2019-11-13 23:17:41 +0300

    initial commit

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views
+
+
+
+urlpatterns = [
+    path('register/', views.register, name ='register'),
+    path('', views.index, name = 'index'),
+    path('', include("django.contrib.auth.urls")),
+    path('django-sb-admin/', include('django_sb_admin.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import RegisterForm
+
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+           
+        return redirect("login")
+
+    else:
+        form = RegisterForm()
+    return render (response, "registration/signup.html", {"form":form})
+
+    
